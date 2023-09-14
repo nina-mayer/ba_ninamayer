0a9aa7589e0770c347fd5efe2d74d3c5b2ffe576
Nina Mayer <nina.mayer4.4@freenet.de>
2023-09-14 12:59:17 +0200

    14.09.23

--- classification_test.R
+++ classification_test.R
@@ -1,6 +1,7 @@
 library(mlr3)
 library(mlr3learners)
-library(kknn)
+library(mlr3viz)
+library(mlr3measures)
 
 ###grade_prediction
 
@@ -13,8 +14,6 @@ gp_learner <- lrn("classif.rpart")
 gp_learner$train(gp_task)
 gp_learner$model
 
-mlr3
-
 #resampling
 cv5 = rsmp("cv", folds = 5)
 gp_rr <- resample(gp_task, gp_learner, cv5)
@@ -23,6 +22,7 @@ gp_rr$prediction()
 
 gp_scores <- gp_rr$score()
 gp_scores[, c("iteration", "classif.ce")]
+
 
 
 ###thoracic_surgery
@@ -46,10 +46,60 @@ ts_scores <- ts_rr$score()
 ts_scores[, c("iteration", "classif.ce")]
 
 
+
+###law_school_admission
+
+#task
+lsa_task <- TaskClassif$new("law_school_admission", law_school_admission, "ugpagt3")
+mlr_tasks$add("law_school_admission", lsa_task)
+
+#decision tree
+lsa_learner <- lrn("classif.rpart")
+lsa_learner$train(lsa_task)
+lsa_learner$model
+
+#resampling
+cv5 = rsmp("cv", folds = 5)
+lsa_rr <- resample(lsa_task, lsa_learner, cv5)
+lsa_rr$aggregate(msr("classif.ce"))
+lsa_rr$prediction()
+
+lsa_scores <- lsa_rr$score()
+lsa_scores[, c("iteration", "classif.ce")]
+
+
+
+###good_customer
+
+#task
+gc_task <- TaskClassif$new("good_customer", good_customer, "bad_client_target")
+mlr_tasks$add("good_customer", gc_task)
+
+#decision tree
+gc_learner <- lrn("classif.rpart")
+gc_learner$train(gc_task)
+gc_learner$model
+
+#resampling
+cv5 = rsmp("cv", folds = 5)
+gc_rr <- resample(gc_task, gc_learner, cv5)
+gc_rr$aggregate(msr("classif.ce"))
+gc_rr$prediction()
+
+gc_scores <- gc_rr$score()
+gc_scores[, c("iteration", "classif.ce")]
+
+
+
 ###benchmark
 
-learners <- list(lrn("classif.rpart"), lrn("classif.kknn"))
-tasks <- list(tsk("grade_prediction"), tsk("thoracic_surgery"))
+learners <- list(lrn("classif.rpart"), lrn("classif.kknn"), lrn("classif.ranger"))
+tasks <- list(tsk("grade_prediction"), tsk("thoracic_surgery"), tsk("law_school_admission"), tsk("good_customer"))
 
 design <- benchmark_grid(tasks, learners, cv5)
 bmr <- benchmark(design)
+
+bmr_ag <- bmr$aggregate()
+bmr_ag[, c("task_id", "learner_id", "classif.ce")]
+
+autoplot(bmr)
--- load_data.R
+++ load_data.R
@@ -15,16 +15,28 @@ ggplot(thoracic_surgery, aes(x = Class)) + geom_bar(fill = c("coral4", "palegree
   theme_bw()
 dev.off()
 
-#good_customer <- readARFF("good_customer")
 
-#jpeg("gcbarplot.jpg", units = "in", width = 14, height = 8, res = 600)
-#ggplot(good_customer, aes(x = bad_client_target)) + geom_bar(fill = c("dodgerblue4", "chocolate3")) + 
-#  ggtitle("Classes for Good Customer Dataset") +
-#  theme(plot.title = element_text(size = 21), axis.text = element_text(size = 16), 
-#        axis.title = element_text(size = 18), legend.text = element_text(size = 16), 
-#        legend.title =  element_text(size = 18)) + 
-#  theme_bw()
-#dev.off()
+
+
+good_customer <- readARFF("good_customer")
+
+good_customer$bad_client_target <- as.factor(good_customer$bad_client_target)
+good_customer$sex <- as.factor(good_customer$sex)
+good_customer$education <- as.factor(good_customer$education)
+good_customer$product_type <- as.factor(good_customer$product_type)
+good_customer$family_status <- as.factor(good_customer$family_status)
+
+jpeg("gcbarplot.jpg", units = "in", width = 14, height = 8, res = 600)
+ggplot(good_customer, aes(x = bad_client_target)) + geom_bar(fill = c("coral4", "palegreen4")) + 
+  ggtitle("Classes for Good Customer Dataset") +
+  theme(plot.title = element_text(size = 21), axis.text = element_text(size = 16), 
+        axis.title = element_text(size = 18), legend.text = element_text(size = 16), 
+        legend.title =  element_text(size = 18)) + 
+  theme_bw()
+dev.off()
+
+
+
 
 grade_prediction <- readARFF("grade_prediction")
 
@@ -39,3 +51,21 @@ ggplot(grade_prediction, aes(x = pass)) + geom_bar(fill = c("dodgerblue4", "choc
   theme_bw()
 dev.off()
 
+
+
+
+law_school_admission_list <- getOMLDataSet(data.id = 43904L)
+law_school_admission <- law_school_admission_list$data
+
+typeof(law_school_admission$ugpagt3)
+law_school_admission$ugpagt3 <- as.factor(law_school_admission$ugpagt3)
+levels(law_school_admission$ugpagt3) <- c(0,1)
+
+jpeg("lsabarplot.jpg", units = "in", width = 14, height = 8, res = 600)
+ggplot(law_school_admission, aes(x = ugpagt3)) + geom_bar(fill = c("dodgerblue4", "chocolate3")) + 
+  ggtitle("Classes for Law School Admission Dataset") +
+  theme(plot.title = element_text(size = 21), axis.text = element_text(size = 16), 
+        axis.title = element_text(size = 18), legend.text = element_text(size = 16), 
+        legend.title =  element_text(size = 18)) + 
+  theme_bw()
+dev.off()
