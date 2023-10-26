e306c37042fa283538dbe89e4e56fa1a640bf698
Nina Mayer <nina.mayer4.4@freenet.de>
2023-10-26 17:28:38 +0200

    26.10.

--- code/classification.R
+++ code/classification.R
@@ -8,7 +8,7 @@ library(tidyverse)
 library(e1071)
 
 
-### classify-function
+### classify - function
 ###
 ### classifies input data according to 4 classifiers and evaluates performance
 ### with 5-fold crossvalidation and 3 performance metrics
@@ -23,7 +23,7 @@ library(e1071)
 classify <- function(data) {
   values <- data.frame(matrix(rep(0, times = 12), ncol = 4, nrow = 3))
   colnames(values) <- c("logreg", "nb", "cart", "knn")
-  rownames(values) <- c("bACC", "fbeta", "recall")
+  rownames(values) <- c("bACC", "f1", "recall")
   output <- list(values)
   cv5 = rsmp("cv", folds = 5)
   
@@ -71,25 +71,89 @@ classify <- function(data) {
   output
 }
 
-smote_cv(data, lrnr, perf) {
+
+### smote_cv - function
+###
+### classifies data with 5 fold cv while applying the smote-algorithm to the 
+### training data in each fold. The classifying learner and the performance measure 
+### is to be specified
+### Input:
+###   data    A dataframe. The data to be classified
+###   lrnr    An object of mlr3-class Learner. The learner to be trained
+###   perf    An object of mlr3-class Measure. The measure of the performance evaluation.
+### Output:
+###   A numeric. The aggregated performance measure
+
+
+smote_cv <- function(data, lrnr, perf) {
   #manual 5-fold CV
   splits <- split(data, sample(rep(1:5, times = c(2000,2000,2000,2000,2000))))
   results <- c(0,0,0,0,0)
   for(i in 1:5){
     #define train and test for split
-    train <- data[-splits[[i]],]
+    train <- data.frame()
+    trainsets <- c(1,2,3,4,5)
+    trainsets <- trainsets[!trainsets %in% c(i)]
+    for(j in 1:4){
+      train <- rbind(train, splits[[j]])
+    }
     test <- splits[[i]]
     
     #apply smote to training data
     list_train <- SMOTE(train[,2:4], train[,1], 5)
     train <- list_train$data
+    train$class <- as.factor(train$class)
     
     #learn model with new training data
-    task <- TaskClassif$new("train", train, "class")
+    task <- TaskClassif$new("train", train, "class", positive = "1")
     learner <- lrnr
     learner$train(task)
     
     #predict test data with learner
-    learner$predict(test)
+    prediction <- learner$predict_newdata(test)
+    results[i] <- prediction$score(perf)
   }
+  mean(results)
 }
+
+
+### classify_smote - function
+###
+### classifies a (imbalanced) data set according to 4 classifiers and evaluates 
+### performance with 5-fold cv and 3 performance metrics while using SMOTE to
+### oversample the minority class
+### Input:
+###   data    A dataframe. The data to be classified
+### Output:
+###   A dataframe with the performance scores for each learner
+
+classify_smote <- function(data) {
+  #define output dataframe
+  output <- data.frame(matrix(rep(0, times = 12), ncol = 4, nrow = 3))
+  colnames(output) <- c("logreg", "nb", "cart", "knn")
+  rownames(output) <- c("bACC", "f1", "recall") 
+  
+  #logreg
+  output[1,1] <- smote_cv(data, lrn("classif.glmnet"), msr("classif.bacc"))
+  output[2,1] <- smote_cv(data, lrn("classif.glmnet"), msr("classif.fbeta"))
+  output[3,1] <- smote_cv(data, lrn("classif.glmnet"), msr("classif.recall"))
+  
+  #naive bayes
+  output[1,2] <- smote_cv(data, lrn("classif.naive_bayes"), msr("classif.bacc"))
+  output[2,2] <- smote_cv(data, lrn("classif.naive_bayes"), msr("classif.fbeta"))
+  output[3,2] <- smote_cv(data, lrn("classif.naive_bayes"), msr("classif.recall"))
+  
+  #cart
+  output[1,3] <- smote_cv(data, lrn("classif.rpart"), msr("classif.bacc"))
+  output[2,3] <- smote_cv(data, lrn("classif.rpart"), msr("classif.fbeta"))
+  output[3,3] <- smote_cv(data, lrn("classif.rpart"), msr("classif.recall"))
+  
+  #knn
+  output[1,4] <- smote_cv(data, lrn("classif.kknn"), msr("classif.bacc"))
+  output[2,4] <- smote_cv(data, lrn("classif.kknn"), msr("classif.fbeta"))
+  output[3,4] <- smote_cv(data, lrn("classif.kknn"), msr("classif.recall"))
+  
+  output
+}
+
+classify_smote(data61)
